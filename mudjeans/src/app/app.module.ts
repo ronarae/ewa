b8be6bed44e6b3532f64f18b03c0d1f852886fc3
Wang Di <wang.jiang@hva.nl>
2020-10-05 11:13:46 +0200

    html css

--- mudjeans/src/app/app.module.ts
+++ mudjeans/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/mainpage/header/header.component';
+import { HomeComponent } from './components/mainpage/home/home.component';
+import { FooterComponent } from './components/mainpage/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
