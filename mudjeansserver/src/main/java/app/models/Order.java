5139d760bc7a150ee4aea247935faee8a5e3f469
Jimi Oost <jimi.dylan.oost@gmail.com>
2020-11-10 16:40:07 +0100

    Base of models built and added back-end implementation for the Jeans

--- mudjeansserver/src/main/java/app/models/Order.java
+++ mudjeansserver/src/main/java/app/models/Order.java
@@ -1,4 +1,65 @@
 package app.models;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public class Order {
+    private int orderId;
+    private User creator;
+    private User reviewer;
+    private String note;
+    private LocalDate date;
+
+    private Map<Jeans, Integer> orderedJeans;
+
+    public Order() {
+    }
+
+    public Order(int orderId, User creator, User reviewer, String note, LocalDate date) {
+        this.orderId = orderId;
+        this.creator = creator;
+        this.reviewer = reviewer;
+        this.note = note;
+        this.date = date;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(User reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
---
+++ mudjeansserver/src/main/java/app/repositories/JeansMockRepository.java
@@ -0,0 +1,68 @@
+package app.repositories;
+
+import app.models.Jeans;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Repository
+public class JeansMockRepository implements JeansRepository {
+    List<Jeans> jeans;
+
+    public JeansMockRepository() {
+        jeans = new ArrayList<>();
+        addRandomJeans();
+    }
+
+    private void addRandomJeans() {
+        save(new Jeans("jeans01", "Mock jeans 1", 30, 10, "All"));
+        save(new Jeans("jeans02", "Mock jeans 2", 4, 5, "Spring"));
+        save(new Jeans("jeans03", "Mock jeans 3", 3, 2, "Autumn"));
+        save(new Jeans("jeans04", "Mock jeans 4", 2, 9, "Autumn"));
+        save(new Jeans("jeans05", "Mock jeans 5", 0, 10, "Summer"));
+    }
+
+    @Override
+    public List<Jeans> findAll() {
+        return jeans;
+    }
+
+    @Override
+    public Jeans save(Jeans j) {
+        int index = jeans.indexOf(j);
+
+        if (index == -1) {
+            jeans.add(j);
+        } else {
+            jeans.set(index, j);
+        }
+
+        return j;
+    }
+
+    @Override
+    public Jeans find(String productCode) {
+        for(Jeans jeansInList : jeans) {
+            if (productCode.equals(jeansInList.getProductCode())) {
+                return jeansInList;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Jeans delete(String productCode) {
+        Iterator<Jeans> it = jeans.iterator();
+        while (it.hasNext()) {
+            Jeans jeans = it.next();
+
+            if(jeans.getProductCode().equals(productCode)) {
+                it.remove();
+                return jeans;
+            }
+        }
+        return null;
+    }
+}
