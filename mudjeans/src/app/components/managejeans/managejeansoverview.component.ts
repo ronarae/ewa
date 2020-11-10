f03f40956354946e3e4b92dd41c535d04285927a
Wang Di <wang.jiang@hva.nl>
2020-11-10 15:03:14 +0100

    voorlopig jeansorder

--- mudjeans/src/app/components/managejeans/managejeansoverview.component.ts
+++ mudjeans/src/app/components/managejeans/managejeansoverview.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {jean} from "../../models/jean";
+import {jean} from '../../models/jean';
 
 @Component({
   selector: 'app-managejeansoverview',
@@ -8,6 +8,7 @@ import {jean} from "../../models/jean";
 })
 export class ManagejeansoverviewComponent implements OnInit {
   productCode = ['MB0001C001D001-28-32', 'MB0001C001D001-29-32', 'MB0001C001D001-30-32', 'MB0001C001D001-32-36', 'MB0001C001D001-36-38'];
+  // tslint:disable-next-line:max-line-length
   description = ['Regular Bryce - Strong Blue', 'Regular Bryce - Authentic Indigo', 'Regular Bryce - Heavy Stone', 'Slim Lassen - Sea Stone', 'Slim Rick - Drip Dry'];
   size = ['W28 L32', 'W29 L32', 'W30 L32', 'W32 L36', 'W36 L38'];
 
