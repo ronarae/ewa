48b836fa80bee0aa938645de39db673a8a836efd
Kaspar Pollet <kasparpollet127@gmail.com>
2020-12-02 15:05:59 +0100

    sendEmail af met een gmail account

--- mudjeansserver/src/main/java/app/models/Notification.java
+++ mudjeansserver/src/main/java/app/models/Notification.java
@@ -2,6 +2,12 @@ package app.models;
 
 import javax.persistence.*;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
 public class Notification {
     @Id
     @Column
@@ -22,13 +28,51 @@ public class Notification {
     @Column(nullable = false)
     private String message;
 
-    public Notification(){}
+    private final String SMTP_SERVER = "smtp.gmail.com ";
+    private final String USERNAME = "mudjeansemailtest@gmail.com";
+    private final String PASSWORD = "ybeyhrmjvrxyoghp";
+    private final String EMAIL_FROM = "mudjeansemailtest@gmail.com";
+
+    public Notification() {}
 
     public Notification(int id, User target, String targetRole, String message) {
         this.id = id;
         this.target = target;
         this.targetRole = targetRole;
         this.message = message;
+    }
+
+    public boolean sendMail() {
+        java.util.Properties prop = System.getProperties();
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", SMTP_SERVER);
+        prop.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(USERNAME, PASSWORD);
+                    }
+                });
+
+        try {
+            Message msg = new MimeMessage(session);
+
+            msg.setFrom(new InternetAddress(EMAIL_FROM));
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(target.getEmail(), false));
+
+            msg.setSubject(order.getOrderId() + " " + order.getNote() + " Created by:" + order.getCreator());
+            msg.setText(message);
+            msg.setSentDate(new Date());
+
+            Transport.send(msg);
+            return true;
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public int getId() {
