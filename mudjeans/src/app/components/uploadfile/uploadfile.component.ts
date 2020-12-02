8ca60d60c46d06c7910f6f1049a139d3a02d8cfc
Wang Di <wang.jiang@hva.nl>
2020-12-02 15:43:14 +0100

    code aanpassing excel inlezen

--- mudjeans/src/app/components/uploadfile/uploadfile.component.ts
+++ mudjeans/src/app/components/uploadfile/uploadfile.component.ts
@@ -31,26 +31,25 @@ export class UploadfileComponent implements OnInit {
                 const wsname: string = wb.SheetNames[0];
                 const ws: XLSX.WorkSheet = wb.Sheets[wsname];
                 this.data = (XLSX.utils.sheet_to_json(ws, {header: 1}));
-                // Delete unwanted information
-                // tslint:disable-next-line:prefer-for-of
+                // Delete first row (empty)
+                this.deleteRow(0, this.data, 1);
                 for (let i = 0; i < this.data.length; i++) {
-                    for (let j = 0; j < 14; j++) {
-                        // Delete column C until P
-                        this.data[i].splice(2, 1);
-                    }
-                    // Delete column V until w temporarily
-                    this.data[i].splice(4, 1);
-                    this.data[i].splice(4, 1);
-                    this.data[i].splice(4, 1);
-                    this.data[i].splice(4, 1);
-                    this.data[i].splice(4, 1);
+                    // delete column C until P
+                    this.deleteColumn(i, 2, this.data, 14);
+                    // delete column S until W
+                    this.deleteColumn(i, 4, this.data, 5);
                 }
-                console.log(this.data);
             };
             reader.readAsBinaryString(target.files[0]);
         } else {
             alert('Uploaded file is not a sale analysis, try again');
         }
+    }
+    deleteRow(row: number, array: [][], deleteCount: number): void{
+        array.splice(row, deleteCount);
+    }
+    deleteColumn(row: number, column: number, array: [][], deleteCount: number): void{
+        array[row].splice(column, deleteCount);
     }
 
     ngOnInit(): void {
