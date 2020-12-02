706ba933b92cfcc325ac69510c43cd71b6199dd6
Wang Di <wang.jiang@hva.nl>
2020-12-02 15:14:02 +0100

    juiste kolommen weergeven

--- mudjeans/src/app/components/uploadfile/uploadfile.component.ts
+++ mudjeans/src/app/components/uploadfile/uploadfile.component.ts
@@ -14,30 +14,43 @@ export class UploadfileComponent implements OnInit {
     }
 
     // tslint:disable-next-line:typedef
-    readPOFile(e: any) {
-        const target: DataTransfer = (e.target) as DataTransfer;
-        if (target.files.length !== 1) {
-            throw new Error('Cannot use multiple files');
-        }
-        const reader: FileReader = new FileReader();
-        // tslint:disable-next-line:no-shadowed-variable
-        reader.onload = (e: any) => {
-            const bstr: string = e.target.result;
-            const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
-            const wsname: string = wb.SheetNames[0];
-            const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-            this.data = (XLSX.utils.sheet_to_json(ws, {header: 1}));
-            // tslint:disable-next-line:prefer-for-of
-            for (let i = 0; i < this.data.length; i++) {
-                const productcode = this.data[i][0];
-                const description = this.data[i][1];
-                const fabric = this.data[i][2];
-                const size = this.data[i][3];
-                const orderfix = this.data[i][4];
-                console.log(productcode);
+    readSalesAnalysis(e: any) {
+        let docname = (document.getElementById('salesAnalysisInput') as HTMLInputElement).value;
+        docname = docname.toLowerCase();
+
+        // Check if the docname contains the "sales" and "analysis"
+        if (docname.includes('sales') && docname.includes('analysis')) {
+            const target: DataTransfer = (e.target) as DataTransfer;
+            if (target.files.length !== 1) {
+                throw new Error('Cannot use multiple files');
             }
-        };
-        reader.readAsBinaryString(target.files[0]);
+            const reader: FileReader = new FileReader();
+            reader.onload = (event: any) => {
+                const bstr: string = event.target.result;
+                const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+                const wsname: string = wb.SheetNames[0];
+                const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+                this.data = (XLSX.utils.sheet_to_json(ws, {header: 1}));
+                // Delete unwanted information
+                // tslint:disable-next-line:prefer-for-of
+                for (let i = 0; i < this.data.length; i++) {
+                    for (let j = 0; j < 14; j++) {
+                        // Delete column C until P
+                        this.data[i].splice(2, 1);
+                    }
+                    // Delete column V until w temporarily
+                    this.data[i].splice(4, 1);
+                    this.data[i].splice(4, 1);
+                    this.data[i].splice(4, 1);
+                    this.data[i].splice(4, 1);
+                    this.data[i].splice(4, 1);
+                }
+                console.log(this.data);
+            };
+            reader.readAsBinaryString(target.files[0]);
+        } else {
+            alert('Uploaded file is not a sale analysis, try again');
+        }
     }
 
     ngOnInit(): void {
