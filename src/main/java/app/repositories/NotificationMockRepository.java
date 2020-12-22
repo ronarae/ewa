844ee1e5459fed5cc67fe5cea287ce48bc034ea9
Shan Hu <shan.hu@hva.nl>
2020-12-22 16:05:32 +0100

    map fix

--- src/main/java/app/repositories/NotificationMockRepository.java
+++
@@ -1,4 +0,0 @@
-package app.repositories;
-
-public class NotificationMockRepository {
-}
--- src/main/java/app/services/StorageException.java
+++
@@ -1,8 +0,0 @@
-package app.services;
-
-import java.io.IOException;
-
-public class StorageException extends Throwable {
-    public StorageException(String s, IOException ex) {
-    }
-}
