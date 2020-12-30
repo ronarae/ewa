bbbae9491c90aedeb3c247a2e719f3bfe932e15d
ronaraederomasrieza <rona.rieza2393@gmail.com>
2020-12-30 18:54:50 +0100

    orderhistory wip

--- mudjeans/src/app/components/orderhistory/orderhistory.component.ts
+++ mudjeans/src/app/components/orderhistory/orderhistory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-orderhistory',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderhistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+  // to delete order in the order history
+  // tslint:disable-next-line:typedef
+  deleteOrderHistory(){
+    const confirmation = confirm('Are you sure you want to delete this order? THIS ACTION CANNOT BE UNDONE!');
+    if (confirmation === true) {
+      this.toastr.success('You have succesfully deleted this order', 'Succesfully deleted!');
+      document.getElementById('closeModal').click();
+    }
   }
 
 }
