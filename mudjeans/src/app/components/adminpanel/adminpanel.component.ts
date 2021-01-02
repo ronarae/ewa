a06009137865147f741ec37231ec8ed902cc00a4
ronaraederomasrieza <rona.rieza2393@gmail.com>
2021-01-02 23:30:10 +0100

    reload add + delete

--- mudjeans/src/app/components/adminpanel/adminpanel.component.ts
+++ mudjeans/src/app/components/adminpanel/adminpanel.component.ts
@@ -47,6 +47,7 @@ export class AdminpanelComponent implements OnInit {
       this.userService.deleteById(user.id);
       this.toastr.success('You have successfully deleted ' + user.email, 'Successfully deleted!');
     }
+    window.location.reload();
   }
 
   // tslint:disable-next-line:typedef
@@ -68,6 +69,7 @@ export class AdminpanelComponent implements OnInit {
     this.userService.save(user);
     this.toastr.success('You have successfully saved this user', 'Successfully saved!');
     this.router.navigate(['/adminpanel']);
+    window.location.reload();
   }
 
 }
