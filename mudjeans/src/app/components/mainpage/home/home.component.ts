9a50c91c630f1b82e9c708ada1d68ec2a440d25c
Kaspar Pollet <kasparpollet127@gmail.com>
2021-01-02 22:31:34 +0100

    header en home verwijdert wat we niet gebruiken / pending orders en low stock zijn uit de database

--- mudjeans/src/app/components/mainpage/home/home.component.ts
+++ mudjeans/src/app/components/mainpage/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderService} from "../../../services/order.service";
+import {Jean} from "../../../models/Jean";
+import {SalesSbService} from "../../../services/sales-sb.service";
+import {Order} from "../../../models/Order";
+import {MatTableDataSource} from "@angular/material/table";
+import {OrderJean} from "../../../models/OrderJean";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  pendingOrders = [];
+  lowStockJeans = [];
+  bestseller = null;
+  totalJeansSold = 0;
+
+  allJeans = [];
+
+  constructor(private orderService: OrderService, private jeansService: SalesSbService) {
+    this.orderService.restGetPendingOrders().subscribe((data) => {
+          // tslint:disable-next-line:prefer-for-of
+          for (let i = 0; i < data.length; i++) {
+            // tslint:disable-next-line:max-line-length
+              const order: Order = new Order(data[i].orderId, data[i].note, data[i].date, data[i].creator.name, data[i].status, data[i].reviewer.name);
+              this.pendingOrders.push(order);
+          }
+        },
+        (error) => {
+          alert('Error:' + error);
+        });
+
+    this.jeansService.restGetJean().subscribe((data) => {
+          for (let i = 0; i < data.length; i++) {
+              const jean: Jean = Jean.trueCopy(data[i]);
+              this.allJeans.push(jean);
+              if (data[i].shouldOrder === true) {
+                this.lowStockJeans.push(jean);
+            }
+          }
+        },
+        (error) => {
+          alert('Error:' + error);
+        });
+  }
 
   ngOnInit(): void {
   }
