5c360f5cf8df1040d40878a0083c3a9776aee924
ronaraederomasrieza <rona.rieza2393@gmail.com>
2021-01-11 20:41:41 +0100

    fe + be test done

--- mudjeans/src/app/services/session-sb.service.spec.ts
+++ mudjeans/src/app/services/session-sb.service.spec.ts
@@ -1,21 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SessionSbService } from './session-sb.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {ToastrModule} from "ngx-toastr";
 import {RouterTestingModule} from "@angular/router/testing";
+import {environment} from "../../environments/environment";
+import {User} from "../models/User";
 
 describe('SessionSbService', () => {
   let service: SessionSbService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, ToastrModule.forRoot(), RouterTestingModule],
     });
     service = TestBed.inject(SessionSbService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // it('should check if the user is logged in',  () => {
+  //   const dummyData = [
+  //     {
+  //       email : 'test@test.test',
+  //       password: 'iamapassword2021!',
+  //     }
+  //   ];
+  //   service.logIn('test@test.test', 'iamapassword2021!').subscribe( (res: User) => {
+  //     expect(res.email).toEqual('test@test.test');
+  //     expect(res.password).toEqual('iamapassword2021!');
+  //   });
+  //   const req = httpMock.expectOne(
+  //       environment.apiUrl + "/auth" + "/login");
+  //   req.flush(dummyData);
+  // });
 });
---
+++ mudjeansserver/src/test/java/app/TestByRona.java
@@ -0,0 +1,84 @@
+package app;
+
+import app.models.User;
+import app.repositories.UserJPARepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestByRona {
+
+    @Autowired
+    private UserJPARepository userJPARepository;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testFindingUser() {
+        User u = userJPARepository.find(20);
+        assertEquals("Dion", u.getName());
+    }
+
+    @Test
+    void testFindingUserByEmail() {
+        //find and get user with an email dion@mudjean.eu
+        ArrayList<User> allUserEmail = new ArrayList<>(userJPARepository.findByQuery("user_find_by_email", "dion@mudjeans.eu"));
+        User u = allUserEmail.get(0);
+        assertEquals("dion@mudjeans.eu", u.getEmail());
+    }
+
+    @Test
+    @DirtiesContext
+    void testUpdatingAUserName() {
+      User u = userJPARepository.find(112);
+      u.setName("JohnTest");
+      userJPARepository.save(u);
+      u = userJPARepository.find(112);
+      assertEquals("JohnTest", u.getName());
+    }
+
+    @Test
+    @DirtiesContext
+    void testRemovingAUser() {
+        User u = userJPARepository.find(113);
+        userJPARepository.delete(u.getId());
+
+        assertNull(userJPARepository.find(113));
+    }
+
+    @Test
+    void testAddingAUser() {
+        User u = new User(0, "TestUser", "viewer", "testpassword", "test@test.test");
+
+        u = userJPARepository.save(u);
+        assertNotNull(u.getId());
+
+        u = userJPARepository.find(u.getId());
+        assertEquals("TestUser", u.getName());
+    }
+
+    @Test
+    void testGetUserById() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .get("/users/", 116))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty());
+    }
+
+}
